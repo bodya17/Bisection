c618522c227792ff400b8ca01b9573d413081a63
bodya17 <levantovychbogdan@gmail.com>
2016-04-20 23:35:55 +0300

    added some user friendliness

--- bisec.c
+++ bisec.c
@@ -11,6 +11,7 @@ using namespace std;
 double f(double x)
 {
     return	x * log10(x + 1) - 0.5;
+    // return x * x * x - x;
 }
 
 int main()
@@ -21,12 +22,27 @@ int main()
 	    , eps;   // точність
 
 	int n = 0;   // кількість ітерацій
-
 	printf("Введіть початок відрізка: ");
 	scanf("%lf", &a);
 
 	printf("Введіть кінець відрізка: ");
 	scanf("%lf", &b);
+
+	double fa = f(a);
+	double fb = f(b);
+
+	if ( fa * fb > 0 ) {
+		printf("%s", "На кінцях інтервала функція повинна змінювати знак, бо алгоритм не спрацює!\n");
+		printf("f(%g) = %s%g\t%sf(%g) = %s%g%s\n", a, RED, fa, BCK, b, RED, fb, BCK);
+
+		do {
+			printf("Введіть початок відрізка: ");
+			scanf("%lf", &a);
+
+			printf("Введіть кінець відрізка: ");
+			scanf("%lf", &b);
+		} while ( f(a) * f(b) > 0);
+	}
 
 	printf("Введіть точність: ");
 	scanf("%lf", &eps);
@@ -43,7 +59,9 @@ int main()
 
 	double fc; // значення функції в середній точці інтервала [a, b]
 
-	printf("%s%-9s%-9s%-9s%-9s%-9s\n", RED, "#", "a", "b", "c", "fc");
+	printf("%sТабуляція:\n%s%-9s%-9s%-9s%-9s%-9s\n", GRN, RED, "#", "a", "b", "c", "fc");
+
+
 	do {
 		n++;
 		c = (a + b) / 2;
@@ -57,7 +75,7 @@ int main()
 
 		printf("%s%-9d%-9g%-9g%-9g%-9g\n", BCK, n, a, b, c, fc);
 
-	} while (fabs(b - a) > eps && fabs(fc) > eps);
+	} while ( fabs(b - a) > eps && fabs(fc) > eps);
 
 	printf("%sКорінь: %s%g\n", GRN, RED, c);
 	printf("%sКількість ітерацій: %s%d\n\n", GRN, RED, n);
